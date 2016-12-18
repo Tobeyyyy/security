cf606dfa9950f1083bd6a9d7f77f80d32bdc97dc
Tobeyyyy <tobey199@gmail.com>
2016-12-17 21:47:58 -0600

    Create header.h

---
+++ header.h
@@ -0,0 +1,23 @@
+/*==============================================
+* Author: Rong Zhang
+*  Date: 11/6/2016
+*  
+*  File name: header.h
+*  Description: saved header
+*  
+*============================================*/
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <stdlib.h>     /* atoi */
+#include <ctype.h>
+#include <algorithm>
+#include <boost/algorithm/string.hpp>  
+
+
+
+using namespace std;
+
